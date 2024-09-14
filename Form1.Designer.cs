4344658113c027576d852e58a3de59e850faaad8
LabUser <LabUser@DESKTOP-BT4131D>
2024-09-14 23:49:28 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace AkariBowens_Sheduling_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.login_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // login_button
+            // 
+            this.login_button.Location = new System.Drawing.Point(326, 68);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(117, 36);
+            this.login_button.TabIndex = 0;
+            this.login_button.Text = "Log in";
+            this.login_button.UseVisualStyleBackColor = true;
+            this.login_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.login_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button login_button;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AkariBowens_Sheduling_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Gets connection string
+            string connectionStr = ConfigurationManager.ConnectionStrings["localdb"].ConnectionString;
+
+            // Makes connection
+            MySqlConnection connect = null;
+
+            try
+            {
+                connect = new MySqlConnection(connectionStr);
+
+                //Opens connection
+                connect.Open();
+
+                // ----- Change later ----- //
+                MessageBox.Show("Connection is open");
+            } catch(MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            } finally
+            {
+                if (connect != null)
+                {
+                    connect.Close();
+                }
+            }
+
+        }
+    }
+}
